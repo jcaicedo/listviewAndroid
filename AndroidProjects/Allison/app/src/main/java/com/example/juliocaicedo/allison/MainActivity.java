c5f8499641f42480af53b26e25bca15ca854a2d2
Julio Caicedo <julio.caicedo@bybeeconcep.com>
2016-06-02 17:53:27 -0400

    new projects

---
+++ AndroidProjects/Allison/app/src/main/java/com/example/juliocaicedo/allison/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.juliocaicedo.allison;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Layout
+
+        RelativeLayout buckysLayout = new RelativeLayout(this);
+        buckysLayout.setBackgroundColor(Color.BLUE);
+
+        //Button
+        Button redButton = new Button(this);
+        redButton.setText("Click me Hoss");
+        redButton.setBackgroundColor(Color.GRAY);
+
+        //username input
+
+        EditText username = new EditText(this);
+
+        redButton.setId(1);
+        username.setId(2);
+
+
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+
+        );
+
+        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+
+        );
+
+        //Give rules to position widgets
+
+        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
+        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        usernameDetails.setMargins(0, 0, 0, 50);
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics());
+
+        username.setWidth(px);
+
+
+
+        //Add widget to layout (button is nov a child of layout
+        buckysLayout.addView(redButton, buttonDetails);
+        buckysLayout.addView(username,usernameDetails);
+
+        //set this actvities content/display to this view
+        setContentView(buckysLayout);
+
+
+
+//        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
